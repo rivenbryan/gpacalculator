a156c7bee6ce6c5b7f3f09af61cfd882d702ef97
rivenbryan <rivenbryan@gmail.com>
2022-12-29 04:25:45 +0800

    update navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,8 +4,39 @@ import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
 import CalculateIcon from '@mui/icons-material/Calculate';
 import Typography from '@mui/material/Typography';
-
+import { React, useContext, useState } from "react";
+import { UserContext } from "../contexts/userContext";
+import IconButton from '@mui/material/IconButton';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import { useNavigate } from 'react-router-dom'
+import { getAuth, signOut } from "firebase/auth";
+import { app } from '../firebase-config';
+// Navbar needs to have the state -> email
 const Navbar = () => {
+  let navigate = useNavigate();
+  const [anchorEl, setAnchorEl] = useState(null);
+  const { userName, setuserName } = useContext(UserContext)
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleLogOut = () => {
+    const auth = getAuth(app);
+    signOut(auth).then(() => {
+
+      localStorage.removeItem("Auth Token")
+      console.log("Sign out successfully")
+      setuserName('')
+      navigate('/')
+    }).catch((error) => {
+    });
+  }
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -18,8 +49,39 @@ const Navbar = () => {
           }}>
             GPA Calculator
           </Typography>
+          {userName !== '' ?
+            <>
+              <Typography>{userName}</Typography>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                color="inherit"
+                onClick={handleMenu}
+              >
+                <AccountCircle />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'bottom',
+                  horizontal: 'left',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'left',
+                }}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleLogOut}>Sign out</MenuItem>
+              </Menu>
+            </>
+            : <Button color="inherit" href="/login">Login</Button>}
 
-          <Button color="inherit" href="/login">Login</Button>
         </Toolbar>
       </AppBar>
 
